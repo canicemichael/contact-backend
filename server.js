23ff8078c30f0d49f1e8a1f48fa32d6734c64529
canicemichael <canicemichael@gmail.com>
2023-06-12 20:31:30 +0100

    ref import path and ref codes in groups

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorHandler");
 const connectDb = require("./config/dbConnection");
+const path = require("path");
 const dotenv = require("dotenv").config();
 
 connectDb();
@@ -8,7 +9,11 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use(errorHandler);
